6dbc0b4b065076deaf4b296fabb4a27f061eb43a
Neka Blair <ourblairfamily@gmail.com>
2024-03-06 10:45:35 -0600

    call to rick and morty api, listing out first 20 characters

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const About = () => {
+  const [char, setChar] = useState([])
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const infoFromApi = await fetchInfo()
+      // setChar()
+    }
+    getInfo()
+  }, [char])
+
+
+  const fetchInfo = async () => {
+    const res = await fetch("https://rickandmortyapi.com/api/character")
+    const data = await res.json()
+    // console.log(data)
+    console.log(data.results)
+    setChar(data.results)
+    
+    // return data
+    // const characters = data.results
+  }
+
+  // fetchInfo()
+
   return (
-    <div>About</div>
+    <>
+      <h1>About</h1>
+      <div>
+        {/* <li {.map()}>Characters</li> */}
+        {char.map((eachChar) => <li key={eachChar.id}>{eachChar.name}</li>)}
+      </div>
+    </>
   )
 }
 
