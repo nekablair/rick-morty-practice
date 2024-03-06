58ccddf382568a520476487609a590ea4de78451
Neka Blair <ourblairfamily@gmail.com>
2024-03-06 10:59:51 -0600

    add characters to correct page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,37 +1,26 @@
 import React, { useEffect, useState } from 'react'
 
 const About = () => {
-  const [char, setChar] = useState([])
+  const [result, setResult] = useState([])
 
   useEffect(() => {
     const getInfo = async () => {
-      const infoFromApi = await fetchInfo()
-      // setChar()
+    const infoFromApi = await fetchInfo()
     }
     getInfo()
-  }, [char])
-
+  }, [])
 
   const fetchInfo = async () => {
-    const res = await fetch("https://rickandmortyapi.com/api/character")
+    const res = await fetch("https://rickandmortyapi.com/api")
     const data = await res.json()
-    // console.log(data)
-    console.log(data.results)
-    setChar(data.results)
-    
-    // return data
-    // const characters = data.results
+    console.log(data)
+    setResult(data)
   }
-
-  // fetchInfo()
 
   return (
     <>
       <h1>About</h1>
-      <div>
-        {/* <li {.map()}>Characters</li> */}
-        {char.map((eachChar) => <li key={eachChar.id}>{eachChar.name}</li>)}
-      </div>
+      {/* {result.map((eachChar) => <li key={eachChar.id}>{eachChar.name}</li>)} */}
     </>
   )
 }
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Characters = () => {
+
+  const [char, setChar] = useState([])
+
+  useEffect(() => {
+    const getInfo = async () => {
+    const infoFromApi = await fetchInfo()
+    }
+    getInfo()
+  }, [])
+
+  const fetchInfo = async () => {
+    const res = await fetch("https://rickandmortyapi.com/api/character")
+    const data = await res.json()
+    console.log(data.results)
+    setChar(data.results)
+  }
+
   return (
-    <div>Characters</div>
+    <>
+      <div>Characters</div>
+      <ul>
+          {char.map((eachChar) => <li key={eachChar.id}>{eachChar.name}</li>)}
+      </ul>
+    </>
   )
 }
 
