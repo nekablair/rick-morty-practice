b7b1c6d8c367af4629d3e1beb59c66a68ab1ea10
Neka Blair <ourblairfamily@gmail.com>
2024-03-06 11:43:38 -0600

    fix navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,69 @@
 import { useState } from 'react'
 import { Outlet, Link } from "react-router-dom"
-// import Navbar from "./Navbar.jsx"
+import Navbar from "./components/Navbar.jsx"
 // import Footer from "./Footer.jsx"
+
+// const Navbar = () => {
+//   <nav>
+//         <h1>Navbar</h1>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/characters">Characters</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact</Link>
+//           </li>
+//         </ul>
+//       </nav>
+// }
+
+
 
 function App() {
 
   return (
     <>
       <nav>
-        <h1>Navbar</h1>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/characters">Characters</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
+          <h1>Navbar</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/characters">Characters</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      <Outlet /> {/* placeholder for whatever is in the router*/}
       <nav>
-        <h1>Footer</h1>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/characters">Characters</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+          <h1>Navbar</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/characters">Characters</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
     </>
   )
 }
