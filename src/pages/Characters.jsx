0be2ada1bc4a0b7dba480c3e5617d7ce6c9b2abf
Neka Blair <ourblairfamily@gmail.com>
2024-03-06 15:06:51 -0600

    card layout

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import CardGroup from 'react-bootstrap/CardGroup'
+import Container from 'react-bootstrap/Container'
 
 const Characters = () => {
 
   const [char, setChar] = useState([])
+  const [image, setImage] = useState('')
 
   useEffect(() => {
     const getInfo = async () => {
     const infoFromApi = await fetchInfo()
     }
     getInfo()
-  }, [])
+  }, [setChar])
 
   const fetchInfo = async () => {
     const res = await fetch("https://rickandmortyapi.com/api/character")
@@ -19,22 +26,44 @@ const Characters = () => {
     setChar(data.results)
   }
 
+// function BasicExample() {
+//   return (
+//     <Card style={{ width: '18rem' }}>
+//       <Card.Img variant="top" src="holder.js/100px180" />
+//       <Card.Body>
+//         <Card.Title>Card Title</Card.Title>
+//         <Card.Text>
+//           Some quick example text to build on the card title and make up the
+//           bulk of the card's content.
+//         </Card.Text>
+//         <Button variant="primary">Go somewhere</Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
   return (
     <>
       <h1>Characters from Rick and Morty</h1>
-          {char.map((eachChar) => 
+        <Row xs={1} md={2} className="g-4">
+        {char.map((eachChar) => 
           <div key={eachChar.id}>
-              <h6>{eachChar.name}</h6>
-              <p>
-              {eachChar.status}
-              </p>
-              <p>
+            <Card border="primary" style={{ width: '26rem'}}>
+              <Card.Img src={eachChar.image} />
+              <Card.Body>
+              <Card.Title>{eachChar.name}</Card.Title>
+              <Card.Text>
+              {eachChar.status} <br />
+              {eachChar.species} <br />
               {eachChar.gender}
-              </p>
-              <img src={eachChar.image} />
-              <Link to={eachChar.url} />
+              </Card.Text>
+              <Button variant="primary" onClick={() =>  <Link to={eachChar.url}></Link> }>Visit {eachChar.name}</Button>
+                
+              </Card.Body>
+              </Card>
               </div>
         )}
+        </Row>
     </>
   )
 }
