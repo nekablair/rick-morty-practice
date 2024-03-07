46361eae057d41d074646a539ad09cea65b69d4a
Neka Blair <ourblairfamily@gmail.com>
2024-03-06 21:22:28 -0600

    center text 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const NotFound = () => {
   return (
     <>
-        <h1>404 - Not Found</h1>
+      <div className='container-fluid text-center p-5'>
+
+        <h1 className='fw-bold'>404 - Not Found</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
+      </div>
     </>
   )
 }
